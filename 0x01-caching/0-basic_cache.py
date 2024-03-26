6075e5872825b8de4e66ed92d4da72b710ab99bc
Fikayomi31 <fikayoogundijo@gmail.com>
2024-03-25 18:03:24 -0700

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Defining a class BasicCache that inherits from BasicCacheing
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Representation of the class BasicCache"""
+
+    def __init__(self):
+        """Initializing the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigning the dict 
+        Args:
+            key: key of the item
+            item: the item of the key
+        """
+        if key and item:
+            self.cache_data[key] = item
+    
+    def get(self, key):
+        """Returning the value of the key or
+        None if key is None
+        Args:
+            key: key of the item
+        """
+        if key:
+            return self.cache_data.get(key, None)
+        
