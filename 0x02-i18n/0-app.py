0fe23ba8fb4334afbe11b02a11915ced61184460
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-05 16:55:10 -0700

    0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+"""A simple flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Return 0-index.html home page"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
